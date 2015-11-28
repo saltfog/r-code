8e2ba09fbff7d552f81885879f02483453cc3404
Casey Jenkins <saltfog@gmail.com>
2015-11-27 21:09:49 -0700

    ggplot heat map

--- SUCCESS Initiative Query/Basic Stats.R
+++ SUCCESS Initiative Query/Basic Stats.R
@@ -1,12 +1,39 @@
 
+# Install ggplot2 package
+install.packages("ggplot2")
 
+# Load the package
+library(ggplot2)
+
+# Read in the data
 data_2013=read.csv("SUCCESS Initiative Query SFY 13.csv",header=TRUE)
 
-#Race Spread
+# Set up the x y variables
+enrolled <- (data_2013$Months.Enrolled)
+served <- (data_2013$Months.Served)
 
-x <- (data_2013$Months.Enrolled)
-y <- (data_2013$Months.Served)
+# Bivariate Analysis
+plot(enrolled,served)
 
+# Bivariate Analysis without outliners
+qqplot(enrolled,served)
 
-plot(x,y)
+# Children in Enrolled
+qplot(data_2013$Months.Enrolled,
+      geom="histogram",
+      binwidth = 1,  
+      main = "Histogram for Months Enrolled", 
+      xlab = "Months Served",  
+      fill=I("blue"), 
+      col=I("red"), 
+      alpha=I(.2),
+      xlim=c(0,40))
 
+mean(data_2013$Months.Enrolled)
+sd(data_2013$Months.Enrolled)
+
+ggplot(data=data_2013, aes(data_2013$Months.Enrolled)) + 
+  geom_histogram(breaks=seq(0, 40, by =1), 
+                 col="red", 
+                 aes(fill=..count..)) +
+  scale_fill_gradient("Count", low = "green", high = "red")

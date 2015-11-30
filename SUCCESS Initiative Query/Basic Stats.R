099bc22aa566ee94278b22c0f35369c17373caad
Casey Jenkins <saltfog@gmail.com>
2015-11-29 21:41:19 -0700

    Added density plot by exit reason

--- SUCCESS Initiative Query/Basic Stats.R
+++ SUCCESS Initiative Query/Basic Stats.R
@@ -1,12 +1,14 @@
 
 # Install ggplot2 package
 install.packages("ggplot2")
+install.packages("sm")
 
 # Load the package
 library(ggplot2)
+library(sm)
 
 # Read in the data
-data_2013=read.csv("SUCCESS Initiative Query SFY 13 Federal Codes.csv",header=TRUE)
+data_2013=read.csv("SUCCESS Initiative Query SFY 13.csv",header=TRUE)
 
 # Set up the x y variables
 enrolled <- (data_2013$Months.Enrolled)
@@ -14,11 +16,28 @@ served <- (data_2013$Months.Served)
 exitcode <- (data_2013$Federal.Code)
 exitreason <-(data_2013$Federal.Exit.Reason)
 
+# Set up the x y variables
+enrolled <- (data_2013$Months.Enrolled)
+served <- (data_2013$Months.Served)
+exitcode <- (data_2013$Federal.Code)
+exitreason <-(data_2013$Federal.Exit.Reason)
+exitage <- (data_2013$Age.at.Exit)
+
+mean(exitage)
+mean(enrolled)
+
+# Scatter Plot
+plot(exitage, enrolled)
+
+# Fit lines
+abline(lm(enrolled~exitage), col="red")
+lines(lowess(exitage,enrolled), col="blue")
+
 # Bivariate Analysis
 plot(enrolled)
 
 # Bivariate Analysis without outliners
-qqplot(enrolled,served)
+qqplot(enrolled,exitage)
 
 # Children in Enrolled
 qplot(data_2013$Months.Enrolled,
@@ -40,13 +59,13 @@ points(seq(min(data_2013$Months.Enrolled), max(data_2013$Months.Enrolled), lengt
 
 ggplot(data=data_2013, aes(data_2013$Months.Enrolled)) + 
   geom_histogram(breaks=seq(0, 40, by =1), 
-                 col="gray", 
-                 aes(fill=..density..)) +
+                 col="black", 
+                 aes(fill=..count..)) +
   scale_fill_gradient("Count", low = "green", high = "red") +
   geom_density(col=1) + labs(title="Histogram for Months Enrolled SFY13") + scale_x_continuous(breaks=seq(0,36,1)) +
   scale_y_continuous(breaks=seq(0,350,10)) +
-  labs(x="Months Served", y="Child Count") +
-lines(density(data_2013$Months.Enrolled, adjust = 2), col = "blue")
+  labs(x="Months Enrolled", y="Child Count") +
+lines(density(data_2013$Age.at.Exit, adjust = 2), col = "blue")
 
 
 ggplot(data=data_2013, aes(data_2013$Months.Served)) + 
@@ -62,4 +81,18 @@ ggplot(data=data_2013, aes(data_2013$Months.Served)) +
 
 ggplot(data_2013, aes(x = data_2013$Months.Served, y = data_2013$Months.Enrolled)) + geom_point()
 
+
+data_2013 <-read.csv("SUCCESS Initiative Query SFY 13 Federal Codes.csv",header=TRUE)
+exitreasons <- factor(data_2013$Federal.Exit.Reason, levels=c(1,2,3,4,5,6,7,8,9), labels=c("1", "2", "3", "4","5","6","7","8","9"))
+colfill<- c(2:(2+length(levels(data_2013$Federal.Exit.Reason))))
+library(sm)
+par(lwd=3)
+sm.density.compare(data_2013$Months.Enrolled, data_2013$Federal.Exit.Reason, xlab="Months Enrolled")
+#legend(locator(1), levels(data_2013$Federal.Exit.Reason), fill=colfill)
+title(main="Distribution by Exit Reason")
+
+
+
+sm.density.compare(enrolled, exitage, xlab="Months Enrolled")
+title(main="Distribution by Exit Age")
 

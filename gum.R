8a04290b63fdf65d779d59dfb5c852b6c1ff99af
Casey Jenkins <caseyjenkins@utah.gov>
2016-01-06 10:51:07 -0700

    added raw data, scatter plot

--- gum.R
+++ gum.R
@@ -4,7 +4,7 @@ rnorm(20)
 # Use a different mean and standard deviation
 x <- rnorm(20, mean=3.7, sd=.1)
 cat(x,sep = "\n")
-
+scatter.smooth(x, y = NULL)
 print(x)
 
 # To check that the distribution looks right, make a histogram of the numbers

572b4d329f4e9c80eb92943c63f029453029601e
Casey Jenkins <caseyjenkins@utah.gov>
2016-01-06 15:48:23 -0700

    New plots

--- gum.R
+++ gum.R
@@ -4,9 +4,16 @@ rnorm(20)
 # Use a different mean and standard deviation
 x <- rnorm(20, mean=3.7, sd=.1)
 cat(x,sep = "\n")
-scatter.smooth(x, y = NULL)
+
 print(x)
 
-# To check that the distribution looks right, make a histogram of the numbers
-x <- rnorm(400, mean=50, sd=10)
 hist(x)
+
+n <- read.csv("gum_raw_data.csv",header=TRUE)
+
+summary(n)
+mean(n)
+min(n)
+max(n)
+
+scatter.smooth(n, y = NULL)

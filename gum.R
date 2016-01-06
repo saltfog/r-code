8ec12cd19994d6d3031968041c9a10a7aa80908b
Casey Jenkins <caseyjenkins@utah.gov>
2016-01-06 09:58:40 -0700

    Gum Stats

---
+++ gum.R
@@ -0,0 +1,12 @@
+rnorm(20)
+
+
+# Use a different mean and standard deviation
+x <- rnorm(20, mean=3.7, sd=.1)
+cat(x,sep = "\n")
+
+print(x)
+
+# To check that the distribution looks right, make a histogram of the numbers
+x <- rnorm(400, mean=50, sd=10)
+hist(x)

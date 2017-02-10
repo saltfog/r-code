a11cca90171a8ea12d1dbd278480ea02a53a52e8
Casey Jenkins <saltfog@gmail.com>
2017-02-09 21:59:10 -0700

    Added new book

---
+++ ShinyApps/word-cloud/ui.R
@@ -0,0 +1,34 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
+fluidPage(
+  # Application title
+  titlePanel("As a Man Thinketh Word Cloud"),
+  
+  sidebarLayout(
+    # Sidebar with a slider and selection inputs
+    sidebarPanel(
+      selectInput("selection", "Choose a book:",
+                  choices = books),
+      actionButton("update", "Change"),
+      hr(),
+      sliderInput("freq",
+                  "Minimum Frequency:",
+                  min = 1,  max = 50, value = 15),
+      sliderInput("max",
+                  "Maximum Number of Words:",
+                  min = 1,  max = 300,  value = 100)
+    ),
+    
+    # Show Word Cloud
+    mainPanel(
+      plotOutput("plot")
+    )
+  )
+)
